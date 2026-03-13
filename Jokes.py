0d9d1a9c897ee198d8b9e589e1876d9b4c8abd80
evelynkagi-star <evelynkagi@seattleacademy.org>
2026-03-13 21:08:43 +0000

    Test

---
+++ Jokes.py
@@ -0,0 +1,18 @@
+print("Do you wanna here a construction joke?")
+input()
+
+
+print("Sorry still working on it!")
+input()
+print("Wanna here another?")
+input()
+
+
+name = input("What is your name?")
+input()
+print(f"Why did {name} cross the road?")
+input()
+
+
+print("To get to the other side!")
+
